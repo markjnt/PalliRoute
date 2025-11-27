9b19de74036656b4ccb8569fd3c15ef706bb5208
Mark <mark.jani@web.de>
2025-11-27 12:40:36 +0100

    Add test message in MainBottomSheet component for layout verification

--- frontend_pwa/src/components/layout/MainBottomSheet.tsx
+++ frontend_pwa/src/components/layout/MainBottomSheet.tsx
@@ -77,6 +77,9 @@ export const MainBottomSheet = forwardRef<MainBottomSheetRef, MainBottomSheetPro
                   onSelectAll={selectAll}
                   onDeselectAll={deselectAll}
                 />
+                <div style={{ padding: '16px 0', textAlign: 'center', color: '#555' }}>
+                  das ist ein test
+                </div>
               </Sheet.Scroller>
             </Sheet.Content>
           </Sheet.Container>
